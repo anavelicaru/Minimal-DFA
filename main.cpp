bf6b49a02084186b151f590d8eeff08252b282e3
Ana Velicaru <ana.velicaru@yahoo.com>
2019-05-02 22:22:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-ifstream f("files.in");
-ofstream g("files.out");
+ifstream f("date.in");
+ofstream g("date.out");
 
 map <char,int> L; // bijectie intre alfabet si numarul de litere
 char alphabet[100];
-int nr_stari,nr_litere;
+int nStari,nLitere;
 
 set <int> operator* (set <int> M1, set <int> M2){
     set <int> intersect;
@@ -18,38 +18,38 @@ set <int> operator* (set <int> M1, set <int> M2){
     return intersect;
 }
 
-void parcurgere (int *viz, int stare, int *automat, int nr_lit, int &nr_fin);
+void parcurgere (int *viz, int stare, int *automat, int nLit, int &nFin);
 int main(){
-    f >> nr_stari >> nr_litere;
+    f >> nStari >> nLitere;
     set <int> fin,nefin;
-    int automat[nr_stari+1][nr_litere];
-    for(int i=0;i<nr_litere;i++){
+    int automat[nStari+1][nLitere];
+    int start;
+    f>>start;
+    int nFin;
+    f>>nFin;
+    int finale[nStari+1];
+    for(int i=0;i<nLitere;i++){
         f >> alphabet[i];
         L[alphabet[i]]=i;
     }
-    int start;
-    f>>start;
-    int nr_fin;
-    f>>nr_fin;
-    int finale[nr_stari+1];
-    for(int i=1;i<=nr_stari;i++) finale[i]=0;
-   	 for(int i=1;i<=nr_fin;i++){
-      		  int x;
-		  f>>x;
-        	  finale[x]=1;
+    for(int i=1;i<=nStari;i++) finale[i]=0;
+    for(int i=1;i<=nFin;i++){
+        int x;
+        f>>x;
+        finale[x]=1;
 
     }
-    int color[nr_stari+1];
-    for(int i=1;i<=nr_stari;i++) if(finale[i]) fin.insert(i);
+    int color[nStari+1];
+    for(int i=1;i<=nStari;i++) if(finale[i]) fin.insert(i);
                                  else nefin.insert(i);
 
-    int nr_tranzitii;
-    f>>nr_tranzitii;
-    for(int i=1;i<=nr_tranzitii;i++){
-        int x,y; char c;
-        f>>x>>c>>y;
-        automat[x][L[c]]=y;
-    }
+    int nArcs; //tranzitii
+    f>>nArcs;
+    for(int i=1;i<=nArcs;i++){
+        int x,y;
+        char c;
+        f >> x >> c >> y;
+        automat[x][L[c]]=y;}
     set < set<int> > P,Q;
     if(nefin.size())
         Q.insert(nefin);
@@ -60,15 +60,15 @@ int main(){
              for(set <int> ::iterator e=it->begin();e!=it->end();e++)
                 color[*e]=col;
          }
-    int nr_color = Q.size();
-    P=Q;
-    for(int x=0;x<nr_litere;x++){
-	    set <int> q[nr_color+1];
-            for(int i=1;i<=nr_stari;i++)
+    int nColors = Q.size();
+    P=Q; //aplicam macar o data algoritmul
+    for(int x=0; x < nLitere; x++){
+	    set <int> q[nColors+1];
+            for(int i=1;i<=nStari;i++)
                 q[color[automat[i][x]]].insert(i);
             set < set<int> > L;
             for(set < set<int> >::iterator it = P.begin(); it!=P.end(); it++)
-                for(int l=1; l<=nr_color; l++){
+                for(int l=1; l<=nColors; l++){
                     set<int> M = (*it) * q[l];
         if(M.size())
                 L.insert(M);
@@ -83,40 +83,41 @@ int main(){
          }
     while(P!=Q)  {
         Q=P;
-        nr_color=Q.size();
-      for(int x=0;x<nr_litere;x++){
-                set<int> q[nr_color+1];
-                for(int i=1;i<=nr_stari;i++)
+        nColors=Q.size();
+      for(int x=0; x<nLitere; x++){
+                set<int> q[nColors+1];
+                for (int i=1;i<=nStari;i++)
                     q[color[automat[i][x]]].insert(i);
                 set< set<int> > L;
                 for(set< set<int> >::iterator it=P.begin();it!=P.end();it++)
-                    for(int l=1;l<=nr_color;l++){
+                    for(int l=1;l<=nColors;l++){
                           set<int>M=(*it)*q[l];
                           if(M.size())
-                          L.insert(M);
+                            L.insert(M);
                         }
              P=L;
         }
+
         col=0;
-        for(set< set<int> >::iterator it=P.begin();it!=P.end();it++)0{
+        for(set< set<int> >::iterator it = P.begin(); it != P.end(); it++){
              ++col;
              for(set<int>::iterator e=it->begin();e!=it->end();e++)
                 color[*e]=col;
          }
 
 }
-
+//ramane sa eliminam nodurile inutile
     int nr_stari2 = P.size();
-    int  auto_min[nr_stari2+1][nr_litere];
+    int  auto_min[nr_stari2+1][nLitere];
     int finale2[nr_stari2+1];
     for(int i=1;i<=nr_stari2;i++)finale2[i]=0;
-   int stare=0,nr_finale=0;
+   int stare=0,nFinale=0;
     for(set< set<int> >::iterator it=P.begin();it!=P.end();it++)
     {
         ++stare;
         if(finale[*(it->begin())])
          finale2[stare]=1;
-        for(int x=0;x<nr_litere;x++)
+        for(int x=0;x<nLitere;x++)
             auto_min[stare][x]=color[automat[*(it->begin())][x]];
     }
 
@@ -124,39 +125,39 @@ int main(){
     for(int i=1;i<=nr_stari2;i++) viz[i]=0;
     int nr_stari3=0;
     int start_min=color[start];
-    parcurg(viz,start_min,(int*)auto_min,nr_litere,nr_stari3);
+    parcurgere(viz,start_min,(int*)auto_min,nLitere,nr_stari3);
     if(!viz[start_min]) {viz[start_min]=1; ++nr_stari3;}
     g<<nr_stari3<<endl;
     for(int i=1;i<=nr_stari2;i++)
         if(viz[i])
             g<<i<<' ';
-    g<<endl<<nr_litere<<endl;
-    for(int i=0;i<nr_litere;i++)
+    g<<endl<<nLitere<<endl;
+    for(int i=0;i<nLitere;i++)
         g<<alphabet[i]<<' ';
     g<<endl;
     g<<start_min<<endl;
     for(int i=1;i<=nr_stari2;i++)
         if(viz[i]&&finale2[i])
-        ++nr_finale;
-    g<<nr_finale<<endl;
+        ++nFinale;
+    g<<nFinale<<endl;
     for(int i=1;i<=nr_stari2;i++)
         if(finale2[i]&&viz[i])
         g<<i<<' ';
     g<<endl;
-    g<<nr_stari3*nr_litere<<endl;
+    g<<nr_stari3*nLitere<<endl;
     for(int i=1;i<=nr_stari2;i++)
-        for(int x=0;x<nr_litere;x++)
+        for(int x=0;x<nLitere;x++)
         if(viz[i])
         g<<i<<' '<<alphabet[x]<<' '<<auto_min[i][x]<<endl;
 
     return 0;
 }
-void parcurgere (int *viz, int stare, int *automat, int nr_lit, int &nr_fin)
+void parcurgere (int *viz, int stare, int *automat, int nr_lit, int &nFin)
 {
     for (int i=0; i<nr_lit; i++)
      if (viz[*((automat + stare * nr_lit)+i)]==0) {
-         nr_fin++;
+         nFin++;
          viz[*((automat+stare*nr_lit)+i)]=1;
-         parcurg(viz,*((automat+stare*nr_lit)+i),automat,nr_lit,nr_fin);
+         parcurgere(viz,*((automat+stare*nr_lit)+i),automat,nr_lit,nFin);
      }
 }
